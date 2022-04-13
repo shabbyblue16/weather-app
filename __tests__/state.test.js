1d4f1e3a49a1362054be285d46120c779d243dbb
J <jbelding16@gmail.com>
2022-04-13 14:37:33 -0400

    Added valid zip code check and alert message

--- __tests__/state.test.js
+++ __tests__/state.test.js
@@ -5,7 +5,8 @@
 /* eslint-disable camelcase */
 /* eslint-disable react/jsx-filename-extension */
 import * as React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { RecoilRoot, useRecoilValue, snapshot_UNSTABLE } from 'recoil';
 import { zipCodeState, enabledState } from '../client/components/state';
 import { SearchBar } from '../client/components';
@@ -18,29 +19,35 @@ function RecoilObserver({ node, onClick }) {
 
 describe('The zipCode state should', () => {
   const onClick = jest.fn();
+  const onChange = jest.fn();
 
-  const { getByTestId } = render(
-    <RecoilRoot>
-      <RecoilObserver node={zipCodeState} onClick={onClick} />
-      <SearchBar />
-    </RecoilRoot>,
-  );
-
+  const setup = () => {
+    const component = render(
+      <RecoilRoot>
+        <RecoilObserver node={zipCodeState} onClick={onClick} onChange={onChange} />
+        <SearchBar />
+      </RecoilRoot>,
+    );
+    const submit = component.getByTestId('search-submit');
+    const input = component.getByTestId('search-input');
+    return {
+      submit,
+      input,
+      ...component,
+    };
+  };
+  const { input, submit } = setup();
   test('be an empty string if nothing is entered into the input', () => {
-    const submit = getByTestId('search-submit');
-
-    fireEvent.click(submit);
+    userEvent.click(submit);
     const initialSnapshot = snapshot_UNSTABLE();
     expect(initialSnapshot.getLoadable(zipCodeState).valueOrThrow()).toBe('');
     expect(initialSnapshot.getLoadable(enabledState).valueOrThrow()).toBe(false);
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  test('change if search is submitted with a 5 digit input', () => {
-    const input = getByTestId('search-input');
-
-    fireEvent.change(input, { target: { value: '97224' } });
-    fireEvent.click(submit);
+  test('change if search is submitted with a 5 digit input', async () => {
+    await userEvent.type(input, '97224');
+    userEvent.click(submit);
     const testingSnapshot = snapshot_UNSTABLE();
     expect(testingSnapshot.getLoadable(zipCodeState).valueOrThrow()).toBe('97224');
     expect(testingSnapshot.getLoadable(enabledState).valueOrThrow()).toBe(true);
